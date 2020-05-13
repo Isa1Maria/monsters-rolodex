5df06a8ccfde9ae3b4d67614ceb8e6717d56fc2c
Isa1Maria <50515191+Isa1Maria@users.noreply.github.com>
2020-05-13 16:49:07 +0100

    Updates

---
+++ precache-manifest.7ac3a81320e4bbe8ea80025b3a12d00a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "785fb8df912792ffae53545407b39957",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1921417df1adaec9e53c",
+    "url": "/monsters-rolodex/static/css/main.f63b4d72.chunk.css"
+  },
+  {
+    "revision": "cae8900dcf0593a9e8f0",
+    "url": "/monsters-rolodex/static/js/2.975f868f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.975f868f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1921417df1adaec9e53c",
+    "url": "/monsters-rolodex/static/js/main.ecffaa13.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
